fa83297e8c96b7c19629790d1090ebf3059e7d68
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-13 19:06:30 -0300

    Adição de Model para pegar atributo.

--- src/main/java/br/com/github/lucasdevrj/mvc/transformacao/controller/OlaController.java
+++ src/main/java/br/com/github/lucasdevrj/mvc/transformacao/controller/OlaController.java
@@ -1,13 +1,15 @@
 package br.com.github.lucasdevrj.mvc.transformacao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OlaController {
 
 	@GetMapping("/ola")
-	public String ola() {
+	public String ola(Model model) {
+		model.addAttribute("nome", "Mundo");
 		return "ola";
 	}
 }
