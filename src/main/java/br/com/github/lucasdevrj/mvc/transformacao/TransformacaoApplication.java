abb98ca79cd7414c8006f9cf17d2062e72857257
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-13 18:35:06 -0300

    Início do projeto.

---
+++ src/main/java/br/com/github/lucasdevrj/mvc/transformacao/TransformacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.lucasdevrj.mvc.transformacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/github/lucasdevrj/mvc/transformacao/TransformacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.lucasdevrj.mvc.transformacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
