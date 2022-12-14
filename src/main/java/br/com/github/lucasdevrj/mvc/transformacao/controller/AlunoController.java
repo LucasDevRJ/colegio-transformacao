00d25e8d6f381d862d36c9d9e33d8165b0ad3e69
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-14 20:29:11 -0300

    Adição da requisição e resposta para a página de formulário.

--- src/main/java/br/com/github/lucasdevrj/mvc/transformacao/controller/AlunoController.java
+++ src/main/java/br/com/github/lucasdevrj/mvc/transformacao/controller/AlunoController.java
@@ -1,8 +1,15 @@
 package br.com.github.lucasdevrj.mvc.transformacao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("aluno")
 public class AlunoController {
 
+	@GetMapping("formulario")
+	public String formulario() {
+		return "aluno/formulario";
+	}
 }
