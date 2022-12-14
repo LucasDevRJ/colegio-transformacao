dbc9f70894932a98939287dc8bfc8c0c982ac155
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-14 16:43:01 -0300

    Injeção de dependencias.

--- src/main/java/br/com/github/lucasdevrj/mvc/transformacao/controller/PrincipalController.java
+++ src/main/java/br/com/github/lucasdevrj/mvc/transformacao/controller/PrincipalController.java
@@ -2,23 +2,25 @@ package br.com.github.lucasdevrj.mvc.transformacao.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.github.lucasdevrj.mvc.transformacao.model.Aluno;
-import jakarta.persistence.EntityManager;
+import br.com.github.lucasdevrj.mvc.transformacao.repository.AlunoRepository;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
 
 @Controller
 public class PrincipalController {
+	
+	@Autowired
+	private AlunoRepository alunoRepository;
 
 	@GetMapping("/principal")
 	public String index(Model model) {
 		
-		
-		
+		List<Aluno> alunos = alunoRepository.recuperaTodosAlunos();
 		model.addAttribute("alunos", alunos);
 		
 		return "principal";
--- src/main/java/br/com/github/lucasdevrj/mvc/transformacao/repository/AlunoRepository.java
+++ src/main/java/br/com/github/lucasdevrj/mvc/transformacao/repository/AlunoRepository.java
@@ -2,11 +2,14 @@ package br.com.github.lucasdevrj.mvc.transformacao.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import br.com.github.lucasdevrj.mvc.transformacao.model.Aluno;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 
+@Repository
 public class AlunoRepository {
 
 	@PersistenceContext
