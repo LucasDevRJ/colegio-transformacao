2b566bb61ad4341ee176ec0dada7f1ffdcefe0ea
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-15 19:08:39 -0300

    Adição do Bean Validation.

--- src/main/java/br/com/github/lucasdevrj/mvc/transformacao/dto/RequisicaoNovoAluno.java
+++ src/main/java/br/com/github/lucasdevrj/mvc/transformacao/dto/RequisicaoNovoAluno.java
@@ -1,16 +1,41 @@
 package br.com.github.lucasdevrj.mvc.transformacao.dto;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 import br.com.github.lucasdevrj.mvc.transformacao.model.Aluno;
 import br.com.github.lucasdevrj.mvc.transformacao.model.Situacao;
 
+
 public class RequisicaoNovoAluno {
 
+	@NotBlank
 	private String nome;
+	
+	@NotBlank
 	private String sobrenome;
+	
+	@NotBlank
+	@Size(min = 1, max = 9)
 	private Integer serie;
+	
+	@NotBlank
+	@Size(min = 5, max = 5)
 	private String matricula;
+	
+	@NotBlank
+	@DecimalMin("0.0")
+	@DecimalMax("10.0")
 	private Double primeiraNota;
+	
+	@NotBlank
+	@DecimalMin("0.0")
+	@DecimalMax("10.0")
 	private Double segundaNota;
+	
 	private Double media;
 	private Situacao situacao;
 	
