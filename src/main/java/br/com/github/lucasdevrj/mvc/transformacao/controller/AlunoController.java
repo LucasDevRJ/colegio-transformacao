77971e5d649319aaabb9154c39de67f9ed60623c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-15 17:41:43 -0300

    Encapsulamento da classe de requisições para novos alunos.

--- src/main/java/br/com/github/lucasdevrj/mvc/transformacao/controller/AlunoController.java
+++ src/main/java/br/com/github/lucasdevrj/mvc/transformacao/controller/AlunoController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.github.lucasdevrj.mvc.transformacao.dto.RequisicaoNovoAluno;
+
 @Controller
 @RequestMapping("aluno")
 public class AlunoController {
--- src/main/java/br/com/github/lucasdevrj/mvc/transformacao/dto/RequisicaoNovoAluno.java
+++ src/main/java/br/com/github/lucasdevrj/mvc/transformacao/dto/RequisicaoNovoAluno.java
@@ -1,5 +1,14 @@
 package br.com.github.lucasdevrj.mvc.transformacao.dto;
 
+import br.com.github.lucasdevrj.mvc.transformacao.model.Situacao;
+
 public class RequisicaoNovoAluno {
 
+	private String nome;
+	private String sobrenome;
+	private Integer serie;
+	private String matricula;
+	private Double primeiraNota;
+	private Double segundaNota;
+	private Situacao situacao;
 }
